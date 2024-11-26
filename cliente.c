f4e8b54a0169534438b1f45b52f4f50db3d54273
Tales Paiva <124717378+talespcalvi@users.noreply.github.com>
2024-11-26 19:29:50 -0300

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,17 +1,42 @@
-#include "genlib.h"
+/**
+ * Arquivo: cliente.c
+ * −−−−−−−−−−−−−−−−−−−−−−−−−
+ * Este é o programa cliente que utiliza a biblioteca "labirinto.h" para
+ * encontrar a saída de um labirinto utilizando algoritmos recursivos de
+ * backtracking.
+ *
+ * Baseado em: Programming Abstractions in C, de Eric S. Roberts.
+ * Capítulo 6: Backtracking Algorithms (pg. 235−245).
+ *
+ * Prof.: Abrantes Araújo Silva Filho
+ * Computação Raiz:
+ * www.computacaoraiz.com.br
+ * www.youtube.com.br/computacaoraiz
+ * github.com/computacaoraiz
+ * twitter.com/ComputacaoRaiz
+ * www.linkedin.com/company/computacaoraiz
+ * www.abrantes.pro.br
+ */
+
 #include "labirinto.h"
+#include "genlib.h"
 #include "simpio.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
 int main(void)
 {
-    ler_mapa_labirinto("arquivo.txt");
+    ler_mapa("labirinto.txt"); // Insira aqui o arquivo do labirinto
+
+    printf("Labirinto original:\n");
+    imprimir_mapa();
 
     if(resolver_labirinto(pegar_posicao_inicial()))
-        printf("As posicoes com X marcam o caminho para saida.\n");
+    {
+        printf("\nAs posicoes com X marcam o caminho para saida.");
+        printf("\nLabirinto resolvido:\n");
+        imprimir_mapa();
+    }
     else
+    {
         printf("Nao existe saida do labirinto.\n");
+    }
 }
